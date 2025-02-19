72cdcc37cec075d80094a0f301d6b74e7d16a52f
David <sumbulovich@gmail.com>
2025-02-19 18:21:31 +0100

    Update

--- projects/storybook-taskbox-lib/src/stories/organisms/inbox-screen/task.service.ts
+++ projects/storybook-taskbox-lib/src/stories/organisms/inbox-screen/task.service.ts
@@ -6,6 +6,13 @@ export class TaskService {
   tasks: WritableSignal<Task[]> = signal([]);
   error: boolean = false;
 
+  loadTasks() {
+    this.tasks.set([
+      { id: '1', title: 'Task 1', state: 'TASK_INBOX' },
+      { id: '2', title: 'Task 2', state: 'TASK_INBOX' },
+      { id: '3', title: 'Task 3', state: 'TASK_INBOX' },
+      { id: '4', title: 'Task 4', state: 'TASK_INBOX' },  
+
   pinTask(id: any) {
     this.tasks.update((tasks: Task[]) => {
       const task = tasks.find((t) => t.id === id);
